5b8e39b4fe5361e768ca3c2b898463347bfbdc2e
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-03-20 11:27:53 -0700

    added tarot card page

---
+++ client/src/components/TarotCard.jsx
@@ -0,0 +1,11 @@
+import CardPlaceholder from "./templates/CardPlaceholder";
+
+export default function TarotCard() {
+  return (
+    <div>
+      <CardPlaceholder />
+      <h3 className='mt-6'>Title</h3>
+    </div>
+  );
+}
+
