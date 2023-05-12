7d61467f5c98dfcf40f49c55f5356ee0e62a71cb
QuevDev <quevdev@outlook.com>
2023-05-12 12:13:35 -0400

    Issue with .rawContnet() in OneCardReading.jsx

--- client/src/components/OneCardReading.jsx
+++ client/src/components/OneCardReading.jsx
@@ -2,15 +2,22 @@ import { useState, useEffect } from 'react';
 
 export default function OneCardReading({ cards }) {
   const [startQuiz, setStartQuiz] = useState(false);
+  const [drawnCard, setDrawnCard] = useState();
 
   useEffect(() => {
     console.log(cards);
   }, [cards]);
 
+  const drawCard = () => {
+    const card = cards[Math.floor(Math.random() * cards.length)];
+    setDrawnCard(card);
+    setStartQuiz(true);
+  };
+
   return (
     <section class='flex flex-col gap-4 m-12 items-center justify-center'>
       <button
-        onClick={() => setStartQuiz(true)}
+        onClick={drawCard}
         class='rounded-md py-2 px-8 bg-palette-c-accent text-palette-c-light'
       >
         Begin Reading
@@ -22,8 +29,17 @@ export default function OneCardReading({ cards }) {
           {cards && (
             <ul>
               {cards.map((card, index) => (
-                <li key={index}>{card.frontmatter.keywords}</li>
+                <li key={index}>
+                  {index}: {card.frontmatter.keywords}
+                </li>
               ))}
+            </ul>
+          )}
+          <p>---</p>
+          {drawnCard && (
+            <ul>
+              <li>{drawnCard.rawContent()}</li>
+              <li>{drawnCard.frontmatter.keywords}</li>
             </ul>
           )}
         </div>
