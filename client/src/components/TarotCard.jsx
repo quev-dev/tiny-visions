e724b9e6ca4f04d2c29564b7960b649b5f0f94b6
QuevDev <quevdev@outlook.com>
2023-06-16 13:17:32 -0400

    Added various transitions & animation for dialogue
    Smooth CSS transition properties (written in client/src/styles/_transitions.css) were made for interactable elements, such as anchor tags and buttons.
    
    The _animations.css file was made for the Tarot dialogue and gives it a quick fade-in effect.

--- client/src/components/TarotCard.jsx
+++ client/src/components/TarotCard.jsx
@@ -1,16 +1,6 @@
-import { useEffect } from 'react';
 import CardPlaceholder from './templates/CardPlaceholder';
 import 'aos/dist/aos.css';
 
-export default function TarotCard({title}) {
-  
-  useEffect(() => {
-    import('aos').then((AOS) => {
-      AOS.default.init();
-    });
-  }, []);
-
-  return (
-    <img data-aos='fade-right' data-aos-once={true} src={`/tarots/${title}.png`} alt={title} />
-  );
+export default function TarotCard({ title }) {
+  return <img src={`/tarots/${title}.png`} alt={title} />;
 }
--- client/src/components/TarotCardButton.jsx
+++ client/src/components/TarotCardButton.jsx
@@ -1,16 +1,28 @@
-import {useStore} from '@nanostores/react'
+import { useEffect } from 'react';
+import { useStore } from '@nanostores/react';
 import { dialogState, dialogContent } from '@src/store/tarotStore';
-import TarotCard from "./TarotCard";
+import TarotCard from './TarotCard';
 
-export default function TartCardButton({info}) {
+export default function TartCardButton({ info }) {
   const $dialogState = useStore(dialogState);
   const $dialogContent = useStore(dialogContent);
-  
+
+  useEffect(() => {
+    import('aos').then((AOS) => {
+      AOS.default.init();
+    });
+  }, []);
+
   return (
-    <button onClick={() => {
-      dialogState.set(!$dialogState)
-      dialogContent.set(info)
-    }}>
+    <button
+      data-aos='fade-right'
+      data-aos-once={true}
+      className='tarot-button bg-palette-c-darker pt-2 pb-8 px-2 rounded-sm'
+      onClick={() => {
+        dialogState.set(!$dialogState);
+        dialogContent.set(info);
+      }}
+    >
       <TarotCard title={info.frontmatter.title} />
       <h3 className='mt-6'>{info.frontmatter.title}</h3>
     </button>
