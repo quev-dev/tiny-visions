57a2cea36779644f8c8510e218f41c2ec11f9877
QuevDev <quevdev@outlook.com>
2023-06-19 19:52:42 -0400

    Added footer with GitHub link to each page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,24 @@
+import Divider from './Divider';
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className='flex flex-col'>
+      <aside className='p-8 rounded-md max-w-5xl mx-auto bg-palette-c-darker'>
+        <p>
+          You can use the link below to visit the GitHub repository for this
+          project.
+        </p>
+        <a
+          className='text-palette-c-accent'
+          target='_blank'
+          rel='noreferrer'
+          href='https://github.com/quev-dev/simple-tarot'
+        >
+          simple-tarot
+        </a>
+      </aside>
+      <div className='my-12'>
+        <Divider />
+      </div>
+    </footer>
+  );
 }
